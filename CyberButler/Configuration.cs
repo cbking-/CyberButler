d3f0c96c27e4b0fd647eed2718eb39064e181fab
Corbin King <corbinking90@gmail.com>
2018-07-14 11:46:04 -0400

    Resolves #23. Using FileSystemWatcher to watch for changes to the config file and reread it when an event is raised.

--- CyberButler/Configuration.cs
+++ CyberButler/Configuration.cs
@@ -13,6 +13,22 @@ namespace CyberButler
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
                 .Build();
+
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = Directory.GetCurrentDirectory();
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Filter = "appsettings.json";
+            
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            Config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
         }
     }
 }
